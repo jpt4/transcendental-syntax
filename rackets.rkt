08a0e572e33250c574f58be3708d08f406813371
jpt4 <jpt4@protonmail.ch>
2025-01-01 23:58:41 +0000

    Initialize rackets.rkt.

---
+++ rackets.rkt
@@ -0,0 +1,11 @@
+;;  rackets.rkt
+;;  20250101Z
+;;  jpt4
+;;  Executable implementation of Transcendental Syntax in Racket
+;;  Racket v8.14
+
+#lang racket
+
+(require minikanren)
+(require minikanren/numbers)
+(require alphakanren) ;;TODO: package alphakanren for Racket.
